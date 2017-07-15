6bed3c0d44f937b362f24d48b5f211ace025a752
gl4d14t0r <gl4d14t0r35@outlook.com>
2017-07-15 16:37:55 +1000

    next

--- test.c
+++ test.c
@@ -2,11 +2,30 @@
 #include <stdbool.h>
 
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 #include "GL/glew.h"
 #include "GL/gl.h"
 
 #define SCREEN_HEIGHT 400
 #define SCREEN_WIDTH 600
+
+SDL_Surface* loadSurface(const char *path, SDL_Surface *screen_surface)
+{
+	SDL_Surface* new_surface = IMG_Load(path);
+	
+	if (new_surface == NULL) {
+		fprintf(stderr, "Unable to load image: %s\n", IMG_GetError());
+		exit(EXIT_FAILURE);
+	} else
+		new_surface = SDL_ConvertSurface(new_surface, screen_surface->format, 0);
+
+	if (new_surface != NULL)
+		return new_surface;
+	else {
+		fprintf(stderr, "Unable to convert surface: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -16,7 +35,14 @@ int main(int argc, char **argv)
 	fprintf(stderr, "Initializing SDL...\n");
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
 		fprintf(stderr, "Initialization failed: %s\n", SDL_GetError());
-	       exit(-1);	
+	       exit(EXIT_FAILURE);	
+	}
+	fprintf(stderr, "Initialization successful\n");
+
+	fprintf(stderr, "Initializing SDL_image...\n");
+	if (!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG)) {
+		fprintf(stderr, "Initialization failed: %s\n", IMG_GetError());
+		exit(EXIT_FAILURE);
 	}
 	fprintf(stderr, "Initialization successful\n");
 
@@ -26,12 +52,23 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
-	if (surface == NULL) {
+	SDL_Surface* screen_surface = SDL_GetWindowSurface(window);
+	if (screen_surface == NULL) {
 		fprintf(stderr, "Surface retrieval failed: %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+	SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
+
+	SDL_Surface* image_surface = loadSurface("randomspaceimage.jpg", screen_surface);
+	
+	SDL_Rect stretch = {
+		0,
+		0,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT
+	};
+
+	SDL_BlitScaled(image_surface, NULL, screen_surface, &stretch);
 
 	while (!quit) {
 		while (SDL_PollEvent(&event)) {
@@ -41,6 +78,8 @@ int main(int argc, char **argv)
 		SDL_UpdateWindowSurface(window);
 	}
 
+	SDL_FreeSurface(screen_surface);
+	SDL_FreeSurface(image_surface);
 	SDL_DestroyWindow(window);
 	SDL_Quit(); fprintf(stderr, "Shutting down SDL...\n");
 	return 0;
